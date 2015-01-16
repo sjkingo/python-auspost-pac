e24dd72c435e72dd2c5bed2e22705553c07ad4c7
Sam Kingston <sam@sjkwi.com.au>
2015-01-17 07:19:05 +1000

    Initial module

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # python-auspost-pac
 
-Python API for Australia Post's Postage Assessment Calculator (pac). This
+Python API for Australia Post's [Postage Assessment Calculator](https://developers.auspost.com.au/apis/pac/getting-started) (pac). This
 includes support for looking up and validating postcodes and localities.
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+from auspost_pac import __version__ as version
+
+setup(
+    name='python-auspost-pac',
+    version=version,
+    license='BSD',
+    author='Sam Kingston',
+    author_email='sam@sjkwi.com.au',
+    description='Python API for Australia Post's Postage Assessment Calculator (pac).',
+    url='https://github.com/sjkingo/python-auspost-pac',
+    install_requires=[
+        'requests',
+    ],
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python',
+    ],
+)
